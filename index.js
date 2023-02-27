940c7f172550d6a3bb9c5f7ce9facad30b4010e3
unknown <dhruvahuja2301@gmail.com>
2023-02-28 01:03:54 +0530

    CORS POLICY

--- index.js
+++ index.js
@@ -56,7 +56,11 @@ app.use(session({
     } 
 }));
 
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 // for json encoded body data
 app.use(express.json());
--- routes/refreshToken.js
+++ routes/refreshToken.js
@@ -7,13 +7,6 @@ if(process.env.NODE_ENV !== "production"){
     require('dotenv').config();
 }
 
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-  
 router.route("/").post(async (req,res)=>{
     try {
         const token = req.session.refreshtoken

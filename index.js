e0b976bd347d6ddb48b56d9e0e2f60f6b3fd1ff5
unknown <dhruvahuja2301@gmail.com>
2023-02-28 00:50:26 +0530

    CORS POLICY

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.use(session({
 }));
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });

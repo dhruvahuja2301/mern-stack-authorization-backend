1622e3326e3cef5c4518a7c1e15b51ed5ba4d629
unknown <dhruvahuja2301@gmail.com>
2023-04-07 17:55:03 +0530

    cors

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const protectedRouter = require("./routes/protected");
 const refreshTokenRouter = require("./routes/refreshToken");
 
 // Mongodb connection
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/login'; 
+const dbUrl = process.env.DB_URL; 
 // const dbUrl = 'mongodb://localhost:27017/login'; 
 mongoose.connect(dbUrl, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false});
 
@@ -67,7 +67,10 @@ app.use(express.json());
 // for url encoded body data
 app.use(express.urlencoded({extended:true}));
 
-app.use(cors({origin: process.env.FRONTEND_URL,credentials: true}));
+app.options(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+}));
 
 // routes
 app.use('/login', loginRouter)
